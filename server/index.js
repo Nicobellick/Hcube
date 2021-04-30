dae180ed0a7254ed1c132f1b77bc71057220ac40
NicoBellick <nicolas.soumier@gmail.com>
2021-05-01 01:52:07 +0200

    Start back structure

--- server/index.js
+++ server/index.js
@@ -18,13 +18,6 @@ app.use(
   })
 )
 
-// connection.connect(function (err) {
-//     if (err) {
-//       console.error('error connecting: ' + err.stack)
-//       return
-//     }
-//     console.log('connected as id ' + connection.threadId)
-//   })
 
   app.get('/', (req, res) => {
       res.send('Hi world')
