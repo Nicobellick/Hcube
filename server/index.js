6d367b2fdf9713b90cfdf7bf9f8d5dda2e03efce
NicoBellick <nicolas.soumier@gmail.com>
2021-05-01 02:45:17 +0200

    add mangoDB & mangoose

--- server/index.js
+++ server/index.js
@@ -2,21 +2,31 @@ const express = require('express')
 const app = express()
 const port = 4242
 const MongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
 
 const url = 'mongodb://localhost:27017'
 const dbName = 'rdv';
 
+mongoose.connect('mongodb://localhost:27017', {useNewUrlParser: true, useUnifiedTopology: true});
 MongoClient.connect(url, function(err, client) {
   console.log("Connecté à MongoDB");
   const db = client.db(dbName);
   client.close();
 });
+
 app.use(express.json())
 app.use(
   express.urlencoded({
     extended: true
   })
 )
+
+db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("connecté à Mongoose")
+});
+
 
 
   app.get('/', (req, res) => {
